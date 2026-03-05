c05b3cafa129cae5b6b7c458f0ddb81cc316780f
denis.vasiliev <denis.vasilev@royalpay.eu>
2026-03-05 16:10:20 +0300

    build: редактирует eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,83 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
-import {defineConfig, globalIgnores} from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import eslintComments from 'eslint-plugin-eslint-comments'
+import prettier from 'eslint-config-prettier'
+
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-    globalIgnores(['dist']),
-    {
-        files: ['**/*.{ts,tsx}'],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite,
-        ],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-        },
+  globalIgnores([
+    'dist',
+    'build',
+    'node_modules',
+    'public',
+    'storybook-static',
+    'webpack/*.js',
+    'eslint.config.js',
+    '**/*.d.ts',
+    'package*.json',
+  ]),
+
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx,json}'],
+
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
     },
 
-    prettier,
-]);
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+      'jsx-a11y': jsxA11y,
+      'eslint-comments': eslintComments,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended[0].rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      ...eslintComments.configs.recommended.rules,
+
+      ...importPlugin.configs.errors.rules,
+      ...importPlugin.configs.warnings.rules,
+      ...importPlugin.configs.typescript.rules,
+
+      ...reactRefresh.configs.vite.rules,
+
+      ...prettier.rules,
+
+      quotes: ['error', 'single', { avoidEscape: true }],
+
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      'react/prop-types': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+])
